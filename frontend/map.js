cc63f22e283faae560b2b4adf267b854af7198eb
Jerrod Lessel <jerrod.lessel@gmail.com>
2026-04-22 23:13:06 -0700

    Update map.js

--- frontend/map.js
+++ frontend/map.js
@@ -652,6 +652,6 @@ document.addEventListener("keydown", function(e) {
 // Push Leaflet zoom controls down so they don't overlap the title
 const zoomControl = document.querySelector(".leaflet-control-zoom");
 if (zoomControl) {
-  zoomControl.style.marginTop = "80px";
+  zoomControl.style.marginTop = "85px";
   zoomControl.style.marginLeft = "15px";
 }
